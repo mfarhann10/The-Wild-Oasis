bbac035fd98b352a1dcec60a08d73a34be8d4aae
mfarhann10 <mfarhann1009@gmail.com>
2025-02-12 23:16:13 +0700

    add the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import styled from 'styled-components';
+import {
+  BrowserRouter,
+  Navigate,
+  replace,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import NewUsers from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-
-const StledDiv = styled.main`
-  background-color: #ddd;
-  padding: 20px;
-`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StledDiv>
-        <Heading as="h1">The Wild Oasis</Heading>
+      <BrowserRouter>
+        <Routes>
+          {/* main element */}
+          <Route index element={<Navigate replace to="dashboard" />} />
 
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert('Check in')}>Check In</Button>
-        <Button onClick={() => alert('Check out')}>Check out</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of Guests" />
-      </StledDiv>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<NewUsers />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
