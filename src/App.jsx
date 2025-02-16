24943a32911f277ee1e7a44b0ceb26cdf010a6be
mfarhann10 <mfarhann1009@gmail.com>
2025-02-17 05:56:40 +0700

    displaying toast (notification) and add some styles to main

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import Login from './pages/Login';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -44,6 +45,28 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      {/* notification */}
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: '16px',
+            backgroundColor: 'var(color-grey-0)',
+            color: 'var(color-grey-700)',
+            padding: '16px 24px',
+            maxWidth: '500px',
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import { formatCurrency } from '../../utils/helpers';
 
 import { deleteCabin } from '../../services/apiCabins';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -61,11 +62,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      toast.success('Cabin successfuly deleted');
       queryClient.invalidateQueries({
         queryKey: ['cabin'],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
