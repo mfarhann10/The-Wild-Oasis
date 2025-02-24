d9479edcda37cde6bb26f9f7a63da1c8a137e71b
mfarhann10 <mfarhann1009@gmail.com>
2025-02-24 16:21:45 +0700

    detecting click outside

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable react/prop-types */
-import { cloneElement, createContext, useContext, useState } from 'react';
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
+import useOutsideClick from '../hooks/useOutsideClick';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -77,10 +84,12 @@ function Open({ children, opens: opensWindowName }) {
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
 
+  const ref = useOutsideClick(close);
+
   if (name !== openName) return null;
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
