470a57bb614d10093ef08ad4aa3b74c37139bc5d
mfarhann10 <mfarhann1009@gmail.com>
2025-03-19 16:04:44 +0700

    protecting route

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Login from './pages/Login';
 import { Toaster } from 'react-hot-toast';
 import Booking from './pages/Booking';
 import Checkin from './pages/Checkin';
+import ProtectedRouted from './ui/ProtectedRouted';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,7 +33,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRouted>
+                <AppLayout />
+              </ProtectedRouted>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
--- src/hooks/authentication/useLogin.js
+++ src/hooks/authentication/useLogin.js
@@ -1,19 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
-export function useLogin(){
-    const navigate = useNavigate()
-    const { mutate: login, isPending: isLogin,} = useMutation({
-        mutationFn: ({email, password}) => loginApi({email, password}),
-        onSuccess: () =>{
-            navigate("/dashboard")
-        },
-        onError: (err) =>{
-            toast.error(err.message)
-        }
-    })
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: login, isPending: isLogin } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      navigate('/dashboard');
+      queryClient.setQueriesData(['user'], user);
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
 
-    return {login, isLogin}
+  return { login, isLogin };
 }
